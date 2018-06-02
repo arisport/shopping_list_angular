3711ad6af3decda6275db4f435437b9a7ffc5bbd
Aris <arispanayiotou@gmail.com>
2018-06-02 15:35:19 +0300

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponentComponent} from './headerComponent/header-component/header-component.component';
+import {ShoppingListComponentComponent} from './headerComponent/shopping-listComponents/shopping-list-component/shopping-list-component.component';
+import {ShoppingListEditComponentComponent} from './headerComponent/shopping-listComponents/shopping-list-edit-component/shopping-list-edit-component.component';
+import {RecipeComponentComponent} from './headerComponent/recipeComponents/recipe-component/recipe-component.component';
+import {RecipeListComponentComponent} from './headerComponent/recipeComponents/recipe-list-component/recipe-list-component.component';
+import {RecipeDetailComponentComponent} from './headerComponent/recipeComponents/recipe-detail-component/recipe-detail-component.component';
+import { RecipeItemComponentComponent } from './headerComponent/recipeComponents/recipe-list-component/recipe-item-component/recipe-item-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    ShoppingListComponentComponent,
+    ShoppingListEditComponentComponent,
+    RecipeComponentComponent,
+    RecipeListComponentComponent,
+    RecipeDetailComponentComponent,
+    RecipeItemComponentComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +27,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
