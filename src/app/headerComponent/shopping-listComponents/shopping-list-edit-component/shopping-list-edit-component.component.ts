2dd7570ddcb941633f07c9eedf8c9fdb3a58003a
Aris <arispanayiotou@gmail.com>
2018-06-02 15:35:48 +0300

    a

---
+++ src/app/headerComponent/shopping-listComponents/shopping-list-edit-component/shopping-list-edit-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list-edit-component',
+  templateUrl: './shopping-list-edit-component.component.html',
+  styleUrls: ['./shopping-list-edit-component.component.css']
+})
+export class ShoppingListEditComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
